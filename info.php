f337dcfcc37bb26b11d6ec8cbcbeb180b13b9cba
souadmaoujoud <101667923+souadmaoujoud@users.noreply.github.com>
2022-05-07 15:56:10 -0400

    creation info.php

---
+++ info.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>activité 9</title>
+</head>
+<body>
+    <h2>Activité 9 Jeux de cartes</h2>
